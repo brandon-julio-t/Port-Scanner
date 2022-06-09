e35e3d1e63e8ad9c788b7eb83f81b4defb148bea
Brandon Julio Thenaro <brandon.julio.t@icloud.com>
2022-06-09 03:28:35 +0000

    solved

--- port_scanner.py
+++ port_scanner.py
@@ -1,8 +1,55 @@
 import socket
+from common_ports import ports_and_services
 
-def get_open_ports(target, port_range):
+
+def get_open_ports(target, port_range, is_verbose=False):
     open_ports = []
 
+    ip = target
 
+    is_ip = ip[0].isnumeric()
+    if not is_ip:
+        try:
+            ip = socket.gethostbyname(ip)
+        except:
+            return 'Error: Invalid hostname'
 
-    return(open_ports)
+    try:
+        socket.inet_aton(ip)
+    except socket.error:
+        return 'Error: Invalid IP address'
+
+    [start, end] = port_range
+    for port in range(start, end + 1):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(0.75)
+        result = s.connect_ex((ip, port))
+        # print(f'{target} => {ip}:{port} => {result}')
+        if result == 0:
+            open_ports.append(port)
+        s.close()
+
+    if not is_verbose:
+        return(open_ports)
+
+    hostname = target
+
+    try:
+        hostname = socket.gethostbyaddr(ip)[0]
+    except:
+        pass
+
+    message = f'Open ports for {hostname}'
+
+    if hostname[0].isalpha():
+        message += f' ({ip})'
+
+    header = 'PORT     SERVICE'
+    body = ''
+    for port in open_ports:
+        service_name = ports_and_services[port]
+        body += str(port).ljust(9) + service_name + '\n'
+    message += f'\n{header}\n{body}'
+    return message.strip()
+
+# print(get_open_ports("104.26.10.78", [440, 450], True))
